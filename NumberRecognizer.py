0aedc183188eb3fedc3b1e60df46a8355ae4a2e9
TianranBao <tianranbao@gmail.com>
2023-10-16 17:37:16 -0700

    Added NumberRecognizer.py and set up libraries that will be used in the project and comments for clarification.

---
+++ NumberRecognizer.py
@@ -0,0 +1,14 @@
+#libraries
+import pandas as pd 
+import matplotlib 
+from matplotlib import pyplot as plt #matplot plotting
+import seaborn as sns #data visualizer 
+import tempfile #allow python to make temporary files 
+import os #OS interface commands 
+import tensorflow as tf 
+import tensorflow_datasets as tfds #will be getting MNIST data from here
+
+matplotlib.rcParams['figure.figsize'] = [9,6] #set matplot figures to 9 inces by 6 inches
+tf.random.set_seed(88)
+
+print("faiusjdfioajsdfasdifoajsdfjaiowejf")
